9270bd264c2f1ddcb6941613fffa2e670312a1b7
Lucas Cid <lucasfelipaaa@gmail.com>
2023-12-22 11:33:58 +0000

    narrowing addicted with identifying types with literal types to make verifications

--- narrowing/index.js
+++ narrowing/index.js
@@ -13,3 +13,28 @@ console.log(array2);
 const array3 = [];
 const emptyArray = [];
 console.log(typeof array3, "tipo");
+function chooseOfOperation(array, operations) {
+    if (operations) {
+        let value; // only for atribute to an variable --> 
+        switch (operations) {
+            case "sum":
+                return array.reduce((i, total) => total + i);
+            case "minus":
+                return value = array.reduce((i, total) => total - i);
+            case "multiply":
+                return value = array.reduce((i, total) => total * i);
+            default:
+                return "preencha com uma operacao";
+        }
+    }
+    else {
+        return "";
+    }
+}
+const operacaoEscolhida = "sum"; //only values that can be accept to type operations
+const arrayValues = [2, 3, 4];
+const result = chooseOfOperation(arrayValues, operacaoEscolhida);
+console.log(result);
+const multiplyOperation = 'multiply';
+console.log(chooseOfOperation(arrayValues, multiplyOperation));
+//try of tuples
--- narrowing/index.ts
+++ narrowing/index.ts
@@ -17,3 +17,38 @@ const array3 : any= []
 const emptyArray : Array<number> = []
 
 console.log(typeof array3, "tipo" )
+
+
+type operations = "sum" | "minus" | "multiply"| undefined;
+
+function chooseOfOperation(array : number [], operations : operations ) : number | string{
+    if(operations){
+        let value : number; // only for atribute to an variable --> 
+        switch(operations){
+            case "sum" :
+                return  array.reduce((i, total) => total + i) 
+            case "minus" :
+                return value = array.reduce((i, total) => total - i)
+            case "multiply" :
+                return value = array.reduce((i, total) => total * i)
+            default :
+                return "preencha com uma operacao"
+        }
+    } else {
+        return ""
+    }
+}
+
+const operacaoEscolhida : operations= "sum" //only values that can be accept to type operations
+
+const arrayValues : Array<number> = [2,3,4]
+
+const result = chooseOfOperation(arrayValues, operacaoEscolhida)
+
+console.log(result)
+
+const multiplyOperation : operations = 'multiply'
+
+console.log(chooseOfOperation(arrayValues, multiplyOperation))
+
+//try of tuples
