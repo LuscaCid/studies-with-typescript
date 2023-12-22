77170a6075dcd1975758ade93143ee507058f912
Lucas Cid <lucasfelipaaa@gmail.com>
2023-12-22 12:56:05 +0000

    new changes at folder of studies with functions ts addicted

--- narrowing/index.ts
+++ narrowing/index.ts
@@ -51,4 +51,60 @@ const multiplyOperation : operations = 'multiply'
 
 console.log(chooseOfOperation(arrayValues, multiplyOperation))
 
-//try of tuples
+// instance of operator to identify if an object variable is like an class passed at second
+
+class User { //its like an normal user in the application
+    name
+    constructor(name : string){
+        this.name = name
+    }
+}
+
+class SuperUser extends User{
+    constructor(name : string){
+        super(name)//passando para  a classe mae o meu name que vai receber
+    }
+    showAdminUser(){
+        console.log(`prazer vê-lo: ${this.name}`)
+    }
+
+}
+const lucas : User = new User("lucas")//instanciando uma variavel de forma annotation e instanciando um objeto User nela
+const Paulo : SuperUser = new SuperUser("paulo")
+
+const greetingUser = (user : object) :void => {
+    if(!user)return console.log('no user passed')
+    if(user instanceof User){
+        return console.log(`ola usuario padrao : ${user.name}`)
+    } else if (user instanceof SuperUser)return console.log(`ola user administrador do sistema ${user.name}`)
+    else return console.log('eoq')
+}
+
+greetingUser(Paulo)
+
+console.log(Paulo.showAdminUser())
+
+//Narrowing para identificar valores para dentro de propiedades com classes 
+
+class Dog {
+    name : string // tipando
+    breed : string | undefined
+    constructor(name : string, breed? : string | undefined){
+        this.name = name //this.name only can receive strings, of course
+        if(breed){
+            this.breed = breed
+        }
+    }
+}
+
+const amora : Dog = new Dog('amora') // breed is an optional value
+
+const pedgree : Dog = new Dog('pitbull enraivado', "pitbull")
+//estou tipando por annotation as variaveis acima e as instanciando como o objeto Dog
+
+
+if('breed' in pedgree)console.log("tem raca, viu filho")
+if('breed' in amora)console.log("tem raca, viu filho")//breed in amora ta undefined
+//este tipo de narrowing vai verificar se o breed que é um campo dentro do objeto Dog 
+//tem valor definido, ou seja, que tenha recebido algo em algum momento...
+console.log(amora.breed)//undefined
